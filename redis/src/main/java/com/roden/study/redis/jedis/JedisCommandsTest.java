8577d9ad742c5e0c3cee4912cf36f513370ab06f
luod@flyingfinancial.hk <huilian1234>
2020-05-19 13:51:42 +0800

    添加命令

--- redis/src/main/java/com/roden/study/redis/jedis/JedisCommandsTest.java
+++ redis/src/main/java/com/roden/study/redis/jedis/JedisCommandsTest.java
@@ -29,8 +29,18 @@ public class JedisCommandsTest {
     }
 
     /**
-    *  set key value 设置一个key 值为 value
-　　     get key 获得key值得value
+     *  set key value 设置一个key 值为 value
+　　  * get key 获得key值得value
+
+     Set KEY VALUE //设置给定key的值，如果key不存在则添加，如果存在则更新值
+     SETEX key Seconds VALUE //为指定的 key 设置值及其过期时间。如果 key 已经存在， SETEX命令将会替换旧的值。
+     Get KEY //获取指定 key 的值
+     GETRANGE KEY start end //获取存储在指定 key 中字符串的子字符串。字符串的截取范围由 start 和 end 两个偏移量决定(包括 start 和 end 在内)。
+     GETSET KEY VALUE //设置给定key的值，并返回旧值
+     SETNX KEY VALUE //给定key不存在时，为key设定值
+     MSET key1 value1 key2 value2 .. keyN valueN //同时设定多个值
+     INCR KEY //给定key的值+1 key对应的值不是整型会报错,value is not an integer or out of range
+     DECR KEY //给定key的值-1 key对应的值不是整型会报错,value is not an integer or out of range
      */
     @Test
     public void str(){
@@ -48,6 +58,11 @@ public class JedisCommandsTest {
      * 相当于1个key 对应一个map
      *   hset key filed value 设置值
      *   hget key filed 　获取值
+     *
+     * Hset key field value //设置给定key 的field的value
+     * Hmset Key field1 value1 field2 value 2 //同时设置多个 field value
+     * Hget key field //获取key中指定feild的value值
+     * Hmget key feild1 feild2 //获取多个feild的值
      */
     @Test
     public void hash(){
@@ -61,9 +76,17 @@ public class JedisCommandsTest {
    　　List 有顺序可重复
 　　  lpush list 1  2  3  4 从左添加元素　
     　rpush list 1 2 3 4    从右添加元素
-    　lrange list 0 -1 (从0 到-1 元素查看：也就表示查看所有)
+    　lrange list 0 -1 (从0 到-1 元素查看：也就表示查看所有，lrange右闭合，也就是包含end下标所代表的项)
     　lpop list （从左边取，删除）
     　rpop list  (从右边取，删除)
+
+
+     Lpush key value1 value2  //将一个或多个值插入List的头部(左)
+     Rpush key value1 value2  //将一个或多个值插入List的尾部(右)
+     Lpop key  //移除列表头的元素
+     Rpop key  //移除列表尾元素
+     BLpop key timeut//移除列表头的元素，如果没有元素会阻塞直到发现可弹出元素或者超时
+     LREM key count value  //根据参数 COUNT 的值，移除列表中与参数 VALUE 相等的元素。 count> 0 从列表头向尾检索移除count个与value相等的元素并移除；count < 0 从尾向头检索移除count个与value相等的元素并移除；count = 0，移除所有与value相等的元素
      */
     @Test
     public void list(){
@@ -80,6 +103,15 @@ public class JedisCommandsTest {
     　 sadd set1 a b c d d (向set1中添加元素) 元素不重复
        smembers set1 （查询元素）
        srem set1 a （删除元素）
+
+     SADD KEY member1 member2 //向集合中添加一个或多个成员
+     SPOP KEY //移除并返回集合中的一个随机元素
+     SRem KEY member1 member2 //移除集合中一个或多个成员
+     SInter key1 key2   //返回给定集合的交集
+     SUnion key1 key2 //返回给定集合的并集
+     SDiff key1 key2 //返回给定集合的差集
+     Smembers Key //返回集合中所有member
+     Sismember Key member //判断member是否在集合中
      */
     @Test
     public void set(){
@@ -98,6 +130,11 @@ public class JedisCommandsTest {
     　　zrange zset1 0 -1 (从小到大)
     　　zrevrange zset1 0 -1 (从大到小)
     　　zincrby zset2 score member (对元素member 增加 score)
+
+     ZAdd Key score1 member1 score2 member2 //向一个集合添加成员，如果成员存在就更新分数
+     ZCount Key min max //获取指定分数区号的成员数量，包括min和max
+     ZRANGE Key start stop [WITHSCORES]//获取指定下标范围的有序成员列表 按照分数排序
+     ZREVRANGE Key start stop [WITHSCORES]//获取指定下标范围的有序成员列表 按照分数倒序
      */
     @Test
     //SortedSet（zset）
